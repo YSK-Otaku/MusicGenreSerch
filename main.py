3de3f70220908fefb0ae64123a2f17eb3ff2b1dc
sasakirione <sasakirione@gmail.com>
2018-11-25 23:47:49 +0900

    BPMが測れるように

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import glob
 def getmfcc(path):
     x, fs = librosa.load(path, sr=44100)
     mfccs = librosa.feature.mfcc(x, sr=fs)
-    print(mfccs.shape,path)
+    print(mfccs[0])
+
+def getBPM(path):
+    x, fs = librosa.load(path, sr=44100)
+    y = librosa.onset.onset_strength(x, fs)
+    bpm = librosa.beat.tempo(onset_envelope=y, sr=fs)
+    print(bpm)
 
 file = glob.glob('*.wav')
 for f in file:
- getmfcc(f)
+    getBPM(f)
 
